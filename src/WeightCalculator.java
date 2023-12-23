ed41445fd125548b095181d5ef4c2c44bc2e201f
parvinder19ptd <120914898+parvinder19ptd@users.noreply.github.com>
2023-12-23 22:12:37 +1100

    Initial commit

---
+++ src/WeightCalculator.java
@@ -0,0 +1,38 @@
+public class WeightCalculator {
+
+    public static double calculateWeight(int age, String gender) {
+        double baseWeight;
+
+        // Define base weights based on gender
+        if (gender.equalsIgnoreCase("male")) {
+            baseWeight = 50.0;
+        } else if (gender.equalsIgnoreCase("female")) {
+            baseWeight = 45.0;
+        } else {
+            System.out.println("Invalid gender");
+            return -1.0; // Return a negative value to indicate an error
+        }
+
+        // Adjust weight based on age
+        if (age < 18) {
+            baseWeight -= 2.0;
+        } else if (age >= 60) {
+            baseWeight += 3.0;
+        }
+
+        return baseWeight;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        int age = 25;
+        String gender = "male";
+        double calculatedWeight = calculateWeight(age, gender);
+
+        if (calculatedWeight >= 0) {
+            System.out.println("Calculated weight for a " + age + "-year-old " + gender + ": " + calculatedWeight + " kg");
+        } else {
+            System.out.println("Error calculating weight");
+        }
+    }
+}
